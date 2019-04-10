0bb5081fa12e1cec3a4c97ec25bfbe849b91aa5e
Max Chou <max9159@gmail.com>
2019-04-10 11:17:46 +0800

    Add project files.

---
+++ Protobuf-net.example/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protobuf_net.example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
