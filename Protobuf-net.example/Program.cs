b45035a8a17627bec66a7c9c99f7deade41f5bf8
Max Chou <max.chou@xuenn.com>
2019-04-10 18:09:10 +0800

    Add "ProtobufSerializer" for the serialization between object and text

--- Protobuf-net.example/Program.cs
+++ Protobuf-net.example/Program.cs
@@ -7,6 +7,7 @@ namespace Protobuf_net.example
     {
         static void Main(string[] args)
         {
+            //1. Write protobuf content into file.
             var userInfo = new UserInfo("max")
             {
                 ID = 1,
@@ -15,19 +16,9 @@ namespace Protobuf_net.example
                 Address = new Address { Line1 = "Taipei", Line2 = "Taiwan"}
             };
 
-            var emptyUserName = new UserInfo("");
-
             using (var file = File.Create("userinfo.bin"))
             {
-                try
-                {
-                    ProtoBuf.Serializer.Serialize(file, userInfo);
-                    ProtoBuf.Serializer.Serialize(file, emptyUserName); //Expected this line will occur an error.
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"Expected occur an error: { e.Message }");
-                }
+                ProtoBuf.Serializer.Serialize(file, userInfo);
             }
 
             UserInfo userInfoFromFile;
@@ -35,7 +26,34 @@ namespace Protobuf_net.example
             {
                 userInfoFromFile = ProtoBuf.Serializer.Deserialize<UserInfo>(file);
             }
-            Console.WriteLine(userInfoFromFile.Username);
+            Console.WriteLine($"Read Protobuf content from file: Username:{ userInfoFromFile.Username }");
+            
+            //2. Try to use [ProtoAfterSerialization] with Validate()
+            var emptyUserName = new UserInfo("");
+            using (var file = File.Create("emptyUserName.bin"))
+            {
+                try
+                {
+                    ProtoBuf.Serializer.Serialize(file, emptyUserName); //Expected this line will occur an error.
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Try the validation mechanism and expected occur an error: { e.Message }");
+                }
+            }
+
+            //3. object to text & text to object
+            var userInfoForSerializer = new UserInfo("test_serializer")
+            {
+                ID = 2,
+                Email = "test.serializer@gmail.com",
+                RegisterTime = DateTime.Now,
+                Address = new Address { Line1 = "Taipei", Line2 = "Taiwan" }
+            };
+            var protobufVal = ProtobufSerializer.SerializeToText(userInfoForSerializer);
+            var userInfoObjFromText = ProtobufSerializer.DeSerializeFromText<UserInfo>(protobufVal);
+
+            Console.WriteLine($"Read Protobuf content from plain text: Username:{ userInfoObjFromText.Username }");
             Console.Read();
         }
     }
---
+++ Protobuf-net.example/ProtobufSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Protobuf_net.example
+{
+    public class ProtobufSerializer
+    {
+        public static string SerializeToText<T>(T obj)
+        {
+            using (var ms = new MemoryStream(1024))
+            {
+                ProtoBuf.Serializer.Serialize(ms, obj);
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+
+        public static T DeSerializeFromText<T>(string text)
+        {
+            var buffer = Convert.FromBase64String(text);
+            using (var ms = new MemoryStream(buffer))
+            {
+                return ProtoBuf.Serializer.Deserialize<T>(ms);
+            }
+        }
+    }
+}
